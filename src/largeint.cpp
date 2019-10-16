66f7d35f530bad19d9ebea8eea5dc0feefd8afa6
ClassesNotStructs <terrariaking1@gmail.com>
2019-10-16 12:49:54 -0700

    cleaning 2

--- src/largeint.cpp
+++ src/largeint.cpp
@@ -3,22 +3,26 @@
 #include <iostream>
 #include <sstream>
 
-//returns index from r->l in a num; eg 76, 0 is 6; 578, 2 is 5
+// returns index from r->l in a num; eg 76, 0 is 6; 578, 2 is 5
 inline unsigned short valAtDigitIndex(const int index, const unsigned long long val)
 {
   return (val % static_cast<int>(round(pow(10, index + 1)))) / pow(10, index);
 }
 
-//returns the numerical value of a character-represented digit if it is within single-digit range, and 10 (error) if not
+// returns the numerical value of a character-represented digit if it is within
+// single-digit range, and 10 (error) if not
 inline unsigned short digitShortFromChar(const char val)
 {
   return (val > 47 && val < 59) ? val - 48 : 10;
 }
 
-char charFromDigitShort(const unsigned short val) { return val + 48; }
+char charFromDigitShort(const unsigned short val)
+{
+  return val + 48;
+}
 
 // checks that each char in a string is in the digit range, returns 0 if not
-bool possessesNonDigitCharacters(const std::string &str)
+bool possessesNonDigitCharacters(const std::string& str)
 {
   bool flag{1};
   for (char i : str)
@@ -30,36 +34,38 @@ bool possessesNonDigitCharacters(const std::string &str)
 
 LargeInt::LargeInt(const unsigned long long init_val)
 {
-  int n{0}; //counter
+  int n{0}; // counter
 
   do
   {
-    m_shorts.push_back(valAtDigitIndex(n++, init_val)); //push back digit spots from least to greatest value
-  } while (init_val / pow(10, n) >= 1);                 //can't exceed the long's value during push_back
+    m_shorts.push_back(valAtDigitIndex(n++, init_val)); // push back digit spots from least to greatest value
+  } while (init_val / pow(10, n) >= 1);                 // can't exceed the long's value during push_back
 }
 
-LargeInt::LargeInt(const std::string &init_string)
+LargeInt::LargeInt(const std::string& init_string)
 {
-  //input checking
+  // input checking
   if (possessesNonDigitCharacters(init_string) || init_string.empty())
   {
     m_shorts = {0};
     return;
   }
 
-  //push the indices back in reverse order
+  // push the indices back in reverse order
   for (size_t index{init_string.length()}; index > 0; index--)
   {
-    m_shorts.push_back(init_string[index - 1] - 48); //already did input checking, so just subtract 48 for conversion
+    m_shorts.push_back(init_string[index - 1] - 48); // already did input checking, so just subtract 48 for conversion
   }
 
-  shrinkPreceedingZeroes(); //retroactively ensure no pointless zeroes are placed in the vector
+  shrinkPreceedingZeroes(); // retroactively ensure no pointless zeroes are
+                            // placed in the vector
 }
 
-//compares the values of two LargeInts by comparing their sizes, then their digits from greatest to least value
-bool operator<(const LargeInt &val_1, const LargeInt &val_2)
+// compares the values of two LargeInts by comparing their sizes, then their
+// digits from greatest to least value
+bool operator<(const LargeInt& val_1, const LargeInt& val_2)
 {
-  //first check sizes and equality
+  // first check sizes and equality
   if (val_1.m_shorts.size() < val_2.m_shorts.size())
   {
     return 1;
@@ -75,7 +81,8 @@ bool operator<(const LargeInt &val_1, const LargeInt &val_2)
     return 0;
   }
 
-  //individual digit comparison, returns result upon finding any difference between digits
+  // individual digit comparison, returns result upon finding any difference
+  // between digits
   for (size_t i{val_1.m_shorts.size()}; i > 0; i--)
   {
     if ((val_1.m_shorts.at(i - 1) > val_2.m_shorts.at(i - 1)))
@@ -88,53 +95,56 @@ bool operator<(const LargeInt &val_1, const LargeInt &val_2)
     }
   }
 
-  return 0; //impossible, but most likely to be equal
+  return 0; // impossible, but most likely to be equal
 }
 
-//determines whether one largeint is greater than another by checking whether it is less than or equal to it
-bool operator>(const LargeInt &val_1, const LargeInt &val_2)
+// determines whether one largeint is greater than another by checking whether
+// it is less than or equal to it
+bool operator>(const LargeInt& val_1, const LargeInt& val_2)
 {
   return !(val_1 < val_2) && !(val_1 == val_2);
 }
 
-//determines whether one largeint is less than or equal to another by checking whether it is greater than it
-bool operator<=(const LargeInt &val_1, const LargeInt &val_2)
+// determines whether one largeint is less than or equal to another by checking
+// whether it is greater than it
+bool operator<=(const LargeInt& val_1, const LargeInt& val_2)
 {
   return !(val_1 > val_2);
 }
 
-//determines whether one largeint is greater than or equal to another by checking whether it is less than it
-bool operator>=(const LargeInt &val_1, const LargeInt &val_2)
+// determines whether one largeint is greater than or equal to another by
+// checking whether it is less than it
+bool operator>=(const LargeInt& val_1, const LargeInt& val_2)
 {
   return !(val_1 < val_2);
 }
 
-//determines equality of two largeints by comparing their vectors
-bool operator==(const LargeInt &val_1, const LargeInt &val_2)
+// determines equality of two largeints by comparing their vectors
+bool operator==(const LargeInt& val_1, const LargeInt& val_2)
 {
   return val_1.m_shorts == val_2.m_shorts;
 }
 
-//applies not to the equality of two largeints
-bool operator!=(const LargeInt &val_1, const LargeInt &val_2)
+// applies not to the equality of two largeints
+bool operator!=(const LargeInt& val_1, const LargeInt& val_2)
 {
   return !(val_1 == val_2);
 }
 
-//overloaded ostream operator
-std::ostream &operator<<(std::ostream &out, const LargeInt &val)
+// overloaded ostream operator
+std::ostream& operator<<(std::ostream& out, const LargeInt& val)
 {
   out << val.representedValue();
   return out;
 }
 
-LargeInt operator+(const LargeInt &intA, const LargeInt &intB)
+LargeInt operator+(const LargeInt& intA, const LargeInt& intB)
 {
   LargeInt sumAB{};
   sumAB.m_shorts.clear();
 
-  const std::vector<unsigned short> &Avec{intA.m_shorts};
-  const std::vector<unsigned short> &Bvec{intB.m_shorts};
+  const std::vector<unsigned short>& Avec{intA.m_shorts};
+  const std::vector<unsigned short>& Bvec{intB.m_shorts};
 
   const size_t Asize{Avec.size()};
   const size_t Bsize{Bvec.size()};
@@ -180,7 +190,7 @@ LargeInt operator+(const LargeInt &intA, const LargeInt &intB)
 
 // consider using a stack to implement the walking up the vector for subtraction
 // from zero
-LargeInt operator-(const LargeInt &intA, const LargeInt &intB)
+LargeInt operator-(const LargeInt& intA, const LargeInt& intB)
 {
   LargeInt tmp_mod{intA};
   if (intA <= intB)
@@ -189,8 +199,8 @@ LargeInt operator-(const LargeInt &intA, const LargeInt &intB)
   std::vector<signed int> tmp_buf_vec{};
 
   // const references with short names for readability
-  const std::vector<unsigned short> &vec_A{intA.m_shorts};
-  const std::vector<unsigned short> &vec_B{intB.m_shorts};
+  const std::vector<unsigned short>& vec_A{intA.m_shorts};
+  const std::vector<unsigned short>& vec_B{intB.m_shorts};
 
   const size_t size_A{vec_A.size()};
   const size_t size_B{vec_B.size()};
@@ -204,10 +214,7 @@ LargeInt operator-(const LargeInt &intA, const LargeInt &intB)
       continue;
     }
 
-    tmp_buf_vec.push_back(
-        vec_A.at(i) -
-        vec_B.at(
-            i)); // otherwise push the result of subtracting the current digits
+    tmp_buf_vec.push_back(vec_A.at(i) - vec_B.at(i)); // otherwise push the result of subtracting the current digits
   }
 
   // if the result of subtracting digits was negative, add 10 and take one from
@@ -250,7 +257,7 @@ LargeInt operator-(const LargeInt &intA, const LargeInt &intB)
   return difAB;
 }
 
-LargeInt operator*(const LargeInt &intA, const LargeInt &intB)
+LargeInt operator*(const LargeInt& intA, const LargeInt& intB)
 {
   LargeInt final_result{0};
 
@@ -282,8 +289,9 @@ LargeInt operator*(const LargeInt &intA, const LargeInt &intB)
   return final_result;
 }
 
-//this algorithm calculates division exactly but inneficiently through a loop and check, so it should only be used when the difference between dividend and divisor is decently large
-LargeInt inefficDiv(const LargeInt &intA, const LargeInt &intB)
+// this algorithm calculates division inneficiently through a loop,
+// so it should only be used when the quotient is small
+LargeInt inefficDiv(const LargeInt& intA, const LargeInt& intB)
 {
   LargeInt i{0};
   while (((i + 1) * intB) <= intA)
@@ -294,33 +302,27 @@ LargeInt inefficDiv(const LargeInt &intA, const LargeInt &intB)
   return i;
 }
 
-LargeInt reverseSliceFrontalNumber(const LargeInt &val, size_t ind_from_left) //zero gives the 1 digit, 1 the 1 then 10, so on
-{
-  LargeInt returnval{0};
-  returnval.m_shorts.clear();
-  returnval.m_shorts.resize(ind_from_left + 1);
-  //std::cout << returnval.m_shorts.size() << returnval.m_shorts.capacity() << val.m_shorts.size();
-  for (size_t i{0}; i <= ind_from_left; i++)
-  {
-    returnval.m_shorts.at(ind_from_left - i) = val.m_shorts.at(val.m_shorts.size() - 1 - i);
-  }
-  return returnval;
-}
-
-LargeInt operator/(const LargeInt &intA, const LargeInt &intB)
+// performs integral division between two largeints
+LargeInt operator/(const LargeInt& intA, const LargeInt& intB)
 {
   LargeInt slice{0};
+  slice.m_shorts.clear();
   LargeInt returnval{0};
+
+  //iterate through A
   for (size_t i{0}; i < intA.m_shorts.size(); i++)
   {
+    //in each iteration, insert the next-rightmost value into slice at 0 index
     slice.m_shorts.insert(slice.m_shorts.begin(), 1, intA.m_shorts.at(intA.m_shorts.size() - i - 1));
-    slice.shrinkPreceedingZeroes();
+    slice.shrinkPreceedingZeroes(); //ensure no stray zeroes remain
     if (intB <= slice)
     {
       LargeInt tmp{inefficDiv(slice, intB)};
+
       slice = slice - tmp * intB;
       for (size_t k{intA.m_shorts.size() - i - 1}; k > 0; k--)
       {
+
         tmp.m_shorts.insert(tmp.m_shorts.begin(), 1, 0);
       }
       returnval = returnval + tmp;
@@ -329,57 +331,73 @@ LargeInt operator/(const LargeInt &intA, const LargeInt &intB)
   return returnval;
 }
 
-LargeInt::operator std::string() const { return representedValue(); }
-
-// appends each digit from least to greatest value
+// private utility used in the case a string is needed and for <<
 std::string LargeInt::representedValue() const
 {
   std::string ret_val{};
+
+  // character represenations of each digit are appended to retval
   for (size_t i{m_shorts.size()}; i > 0; i -= 1)
   {
     ret_val += charFromDigitShort(m_shorts.at(i - 1));
   }
+
   return ret_val;
 }
 
-LargeInt *LargeInt::shrinkPreceedingZeroes()
+// private utility function which removes the preceeding zeroes from the vector;
+// can be chained
+LargeInt* LargeInt::shrinkPreceedingZeroes()
 {
+  // while the last value in the vector is zero
   while (!m_shorts[m_shorts.size() - 1])
   {
+    // don't remove the zero if it would clear the vector
     if (m_shorts.size() == 1 && m_shorts.at(0) == 0)
     {
       return this;
     }
-    m_shorts.pop_back();
+    m_shorts.pop_back(); // remove the zero if not the number's value
   }
-  return this;
+  return this; // return this so it can be chained if desired
 }
 
-// return reference to result
-LargeInt &LargeInt::operator++()
+// prefix increment overload
+LargeInt& LargeInt::operator++()
 {
+  // add one
   *this = *this + 1;
+  // return reference
   return *this;
 }
 
-// return copy of previous
+// postfix increment overload
 LargeInt LargeInt::operator++(int)
 {
+  // copy value
   LargeInt val = *this;
+  // add one
   *this = *this + 1;
+  // return copy
   return val;
 }
 
-// return reference to result
-LargeInt &LargeInt::operator--()
+// prefix decrement overload
+LargeInt& LargeInt::operator--()
 {
+  // subtract one
   *this = *this - 1;
+  // return ref
   return *this;
 }
-// return copy of previous
+
+// postfix decrement overload
 LargeInt LargeInt::operator--(int)
 {
+  // copy value
   LargeInt val = *this;
+  // subtract one
   *this = *this - 1;
+  // return copy
   return val;
 }
