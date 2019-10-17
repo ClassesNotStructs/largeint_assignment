eb40ca3b169258b6acc3b70d6e5504eb7d6a9704
ClassesNotStructs <terrariaking1@gmail.com>
2019-10-16 19:02:23 -0700

    idk

--- inc/largeint.h
+++ inc/largeint.h
@@ -15,10 +15,6 @@ public:
   // cast to std::string is same as calling representedvalue
   operator std::string() const;
 
-  size_t getsize()
-  {
-    return m_shorts.size();
-  }
   // return reference to result
   LargeInt& operator++();
   // return copy of previous
@@ -31,11 +27,21 @@ public:
   //just an exponent operator, since it seemed like it would be nice to have
   LargeInt& operator^(const LargeInt& exponent);
 
+  // compares the values of two LargeInts by comparing their sizes, then their
+  // digits from greatest to least value
   friend bool operator<(const LargeInt& val_1, const LargeInt& val_2);
+  // determines whether one largeint is greater than another by checking whether
+  // it is less than or equal to it
   friend bool operator>(const LargeInt& val_1, const LargeInt& val_2);
+  // determines whether one largeint is less than or equal to another by checking
+  // whether it is greater than it
   friend bool operator<=(const LargeInt& val_1, const LargeInt& val_2);
+  // determines whether one largeint is greater than or equal to another by
+  // checking whether it is less than it
   friend bool operator>=(const LargeInt& val_1, const LargeInt& val_2);
+  // determines equality of two largeints by comparing their vectors
   friend bool operator==(const LargeInt& val_1, const LargeInt& val_2);
+  // applies not to the equality of two largeints
   friend bool operator!=(const LargeInt& val_1, const LargeInt& val_2);
 
   friend LargeInt operator+(const LargeInt& intA, const LargeInt& intB);
