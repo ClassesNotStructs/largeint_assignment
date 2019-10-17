4fe405c5025f98330749fe192e3949d862b4152c
ClassesNotStructs <terrariaking1@gmail.com>
2019-10-16 20:06:21 -0700

    final

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,10 @@
 
 int main()
 {
-  // LargeInt test{100010};
-  // std::cout << test.representedValue() << std::endl;
-  LargeInt test1{"1123409"};
-  LargeInt test2{"1824"};
-  LargeInt test3{"1234"};
-  //std::cout << ((test1 * test2 / test2) == test1) << std::endl;
+  LargeInt test1{"112"};
+  LargeInt test2{"21273849213847128374812374812348723842144"};
 
-  LargeInt val{(test1 - test2)};
+  LargeInt val{(test1 * test2)};
   std::cout << val;
   return 0;
 }
